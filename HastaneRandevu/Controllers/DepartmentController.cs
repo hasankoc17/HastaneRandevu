39f11977cf244d666a7dd6601bb0f75e11f2beea
hasankoc17 <hasan.koc7@ogr.sakarya.edu.tr>
2023-12-23 22:57:25 +0300

    Genel yapı icin birkac class ekleni

---
+++ HastaneRandevu/Controllers/DepartmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneRandevu.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HastaneRandevu/Models/Departments.cs
@@ -0,0 +1,6 @@
+﻿namespace HastaneRandevu.Models
+{
+    public class Departments
+    {
+    }
+}
