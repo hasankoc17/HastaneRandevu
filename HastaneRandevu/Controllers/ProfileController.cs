3330af9afc40929aabcb14f3a4956696a2590d96
hasankoc17 <hasan.koc7@ogr.sakarya.edu.tr>
2023-12-29 22:12:55 +0300

    Profil düzenlemesi için controller.

---
+++ HastaneRandevu/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneRandevu.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
