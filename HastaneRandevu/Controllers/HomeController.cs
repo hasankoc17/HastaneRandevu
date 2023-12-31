8a861913ac3570972c1e9925fdc0bde188f32592
hasankoc17 <hasan.koc7@ogr.sakarya.edu.tr>
2023-12-31 22:31:47 +0300

    frontend

--- HastaneRandevu/Controllers/HomeController.cs
+++ HastaneRandevu/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace HastaneRandevu.Controllers
             return View();
         }
 
-        public IActionResult Randevu()
-        {
-            return View();
-        }
+       
         public IActionResult Privacy()
         {
             return View();
